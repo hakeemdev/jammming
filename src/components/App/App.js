3b0915a8865141abaf3a747cdda720fa4b755d17
hakeem <hakeemali34@gmail.com>
2022-08-05 11:31:55 -0500

    -Passed down Playlist to Tracklist -Added functionality that adds and removes tracks to and from playlists

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,8 +24,44 @@ class App extends React.Component {
         artist: 'artist3',
         album: 'album3',
         id: 3
+      }],
+      playlistName: 'My Playlist',
+      playlistTracks: [{
+        name: 'playlistName1',
+        artist: 'playlistArtist1',
+        album: 'playlistAlbum1',
+        id: 1
+      },{
+        name: 'playlistName2',
+        artist: 'playlistArtist2',
+        album: 'playlistAlbum2',
+        id: 2
+      },{
+        name: 'playlistName3',
+        artist: 'playlistArtist3',
+        album: 'playlistAlbum3',
+        id: 3
       }]
-      }
+      };
+      this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
 
@@ -36,8 +72,13 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults}/>
-          <Playlist />
+          <SearchResults 
+            searchResults={this.state.searchResults}
+            onAdd={this.addTrack} />
+          <Playlist 
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks}
+            onRemove={this.removeTrack} />
         </div>
       </div>
     </div>
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -5,11 +5,17 @@ import Track from '../Track/Track'
 import './TrackList.css'
 
 class TrackList extends React.Component {
+
     render() {
         return (
             <div className="TrackList">
                 {this.props.tracks.map(track => {
-                        return <Track track={track}key={track.id} />
+                        return <Track 
+                        track={track}
+                        key={track.id}
+                        onAdd={this.props.onAdd}
+                        onRemove={this.props.onRemove}
+                        isRemoval={this.props.isRemoval} />
                     })}
             </div>  
         )

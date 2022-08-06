3564915b0d9a5001eef2458d162ed0b77c768928
hakeem <hakeemali34@gmail.com>
2022-08-06 10:33:22 -0500

    Completed Jammming App!

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -11,8 +11,7 @@ class SearchResults extends React.Component {
                 <h2>Results</h2>
                 <TrackList 
                 tracks={this.props.searchResults}
-                onAdd={this.props.onAdd}
-                isRemoval={false} />
+                onAdd={this.props.onAdd} />
             </div>
         )
     }
